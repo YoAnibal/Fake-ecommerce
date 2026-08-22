e3390b148e5ae735ef5e220b2822507e639251fc
YoAnibal <ajmartinezr0516@gmail.com>
2026-08-22 15:49:35 -0500

    Fix image paths in products array
    Updated image paths for products to remove 'images/' prefix.

--- products.js
+++ products.js
@@ -4,41 +4,41 @@ const products = [
     name: "Sneakers Blancos",
     category: "Calzado",
     price: 59.99,
-    image: "images/sneakers-blancos.jpg"
+    image: "sneakers-blancos.jpg"
   },
   {
     id: "shoe-002",
     name: "Botas Urbanas",
     category: "Calzado",
     price: 89.99,
-    image: "images/botas-urbanas.jpg"
+    image: "botas-urbanas.jpg"
   },
   {
     id: "shoe-003",
     name: "Tenis Running",
     category: "Calzado",
     price: 74.99,
-    image: "images/tenis-running.jpg"
+    image: "tenis-running.jpg"
   },
   {
     id: "jean-001",
     name: "Jean Slim Azul",
     category: "Jeans",
     price: 49.99,
-    image: "images/jean-slim.jpg"
+    image: "jean-slim.jpg"
   },
   {
     id: "jean-002",
     name: "Jean Negro Recto",
     category: "Jeans",
     price: 54.99,
-    image: "images/jean-negro.jpg"
+    image: "jean-negro.jpg"
   },
   {
     id: "jean-003",
     name: "Jean Mom Fit",
     category: "Jeans",
     price: 52.99,
-    image: "images/jean-mom.jpg"
+    image: "jean-mom.jpg"
   }
 ];
